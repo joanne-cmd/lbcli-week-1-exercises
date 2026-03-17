f92f91eccf711d52cc589521fc14cd3a8359bea8
joanne_cmd <joanne.m.muthoni@gmail.com>
2026-03-17 22:58:30 +0300

    feat:  had fun with bitcoin commands

--- submission/05.sh
+++ submission/05.sh
@@ -1,2 +1,5 @@
 # Check the total amount in the wallet.
+
+RPCADDR=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress)
+bitcoin-cli -regtest generatetoaddress 101 "$RPCADDR" >/dev/null
 bitcoin-cli -regtest -rpcwallet=builderswallet getbalance
